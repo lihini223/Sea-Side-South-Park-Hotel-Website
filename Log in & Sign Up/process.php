7e831d522c547db4874e1f9c4e835dbc3bdb6dfc
lihini223 <lihininisansala95@gmail.com>
2020-08-23 11:28:12 +0530

    update file

--- Log in & Sign Up/process.php
+++ Log in & Sign Up/process.php
@@ -4,12 +4,15 @@ $email = $_POST['lemail'];
 $password = $_POST['lpassword'];
 
 //to prevent my sql injection
-$username = stripcslashes($email); 
+$email = stripcslashes($email); 
 $password = stripcslashes($password); 
-$username = mysql_real_escape_string($email); 
+$email = mysql_real_escape_string($email); 
 $password = mysql_real_escape_string($password); 
 
 //connect to the server
 
+//query
+$qry = "";
+$result = mysql_query();
 
 ?>
