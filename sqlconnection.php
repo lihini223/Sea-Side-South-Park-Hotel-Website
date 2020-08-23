00880e44e612e75c89dcf0c497882e8cdd4018c6
lihini223 <lihininisansala95@gmail.com>
2020-08-23 11:27:42 +0530

    update file

---
+++ sqlconnection.php
@@ -0,0 +1,12 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "hoteldb";
+
+//connect to the server and select database
+$con = mysqli_connect($server,$username,$password,$database);
+
+
+?>
